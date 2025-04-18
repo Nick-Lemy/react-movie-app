df50ff488bcc5ac583848eeb9cff04dffad212f3
Nick-Lemy <n.kayiranga@alustudent.com>
2025-04-18 13:07:54 +0200

    implement search fonctionality

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Search from './components/Search'
 import { React, useState, useEffect } from 'react';
 import Spinner from './components/Spinner';
 import MovieCard from './components/MovieCard';
+import { useDebounce } from 'react-use';
 
 const API_BASE_URL = 'https://api.themoviedb.org/3';
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
@@ -18,16 +19,18 @@ function App() {
   const [searchTerm, setSearchTerm] = useState('');
   const [movieList, setMovieList] = useState([])
   const [isLoading, setIsLoading] = useState(false)
+  const [debouncedSearch, setDebouncedSearch] = useState('')
+  useDebounce(()=>setDebouncedSearch(searchTerm), 500, [searchTerm])
 
-  const fetchMovies = async () => {
+  const fetchMovies = async (query = '') => {
     setIsLoading(true)
     try {
-      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc}`
+      const endpoint = query ? `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}` : `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`
       const response = await fetch(endpoint, API_OPTIONS)
       if (!response.ok) throw new Error("Failed to fetch movies");
 
       const data = await response.json()
-      if(data.Response === 'False') {
+      if (data.Response === 'False') {
         setErrorMessage(data.error || 'Failed to fetch movies')
         setMovieList([])
       }
@@ -41,8 +44,8 @@ function App() {
   }
 
   useEffect(() => {
-    fetchMovies()
-  }, [])
+    fetchMovies(debouncedSearch)
+  }, [debouncedSearch])
 
   return (
     <main>
@@ -60,12 +63,12 @@ function App() {
           {
             isLoading ? (
               <Spinner />
-            ) : errorMessage ?(
+            ) : errorMessage ? (
               <p className='text-red-500'>{errorMessage}</p>
             ) : (
               <ul>
-                {movieList.map(movie=>(
-                  <MovieCard key={movie.id} movie={movie}/>
+                {movieList.map(movie => (
+                  <MovieCard key={movie.id} movie={movie} />
                 ))}
               </ul>
             )
