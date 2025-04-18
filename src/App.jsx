f29bfacb57002bb612c8f6e29b12c6d88e05de97
Nick-Lemy <n.kayiranga@alustudent.com>
2025-04-18 14:30:12 +0200

    trending movies functionality added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { React, useState, useEffect } from 'react';
 import Spinner from './components/Spinner';
 import MovieCard from './components/MovieCard';
 import { useDebounce } from 'react-use';
+import { getTrendingMovies, updateSearchCount } from './appwrite';
 
 const API_BASE_URL = 'https://api.themoviedb.org/3';
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
@@ -18,9 +19,10 @@ function App() {
   const [errorMessage, setErrorMessage] = useState('')
   const [searchTerm, setSearchTerm] = useState('');
   const [movieList, setMovieList] = useState([])
+  const [trendingMovies, setTrendingMovies] = useState([])
   const [isLoading, setIsLoading] = useState(false)
   const [debouncedSearch, setDebouncedSearch] = useState('')
-  useDebounce(()=>setDebouncedSearch(searchTerm), 500, [searchTerm])
+  useDebounce(() => setDebouncedSearch(searchTerm), 500, [searchTerm])
 
   const fetchMovies = async (query = '') => {
     setIsLoading(true)
@@ -35,6 +37,9 @@ function App() {
         setMovieList([])
       }
       setMovieList(data.results || [])
+      if (query && data.results.length > 0) {
+        await updateSearchCount(query, data.results[0])
+      }
     } catch (error) {
       console.error(`Error while fetching data: ${error}`)
       setErrorMessage('Error fetching movies. Please try again later.')
@@ -42,6 +47,19 @@ function App() {
       setIsLoading(false)
     }
   }
+
+  const loadTrendingMovies = async () => {
+    try {
+      const movies = await getTrendingMovies();
+      setTrendingMovies(movies)
+    } catch (error) {
+      console.error(`Error fetching trending movies: ${error}`)
+    }
+  }
+
+  useEffect(() => {
+    loadTrendingMovies()
+  }, [])
 
   useEffect(() => {
     fetchMovies(debouncedSearch)
@@ -57,9 +75,21 @@ function App() {
           <h1>Find <span className='text-gradient'>Movies</span> You'll Enjoy Without the Hassle</h1>
           <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
         </header>
+        {trendingMovies.length > 0 && (
+          <section className='trending'>
+            <h2 className='text-white'>Trending Movies</h2>
+            <ul>
+              {trendingMovies.map((movie, index)=>(
+                <li key={movie.$id}>
+                  <p>{index + 1}</p>
+                  <img src={movie.poster_url} alt={movie.title} />
+                </li>
+              ))}
+            </ul>
+          </section>
+        )}
         <section className='all-movies'>
-          <h2 className='mt-[20px]'>All Movies</h2>
-          {/* {errorMessage && <p className='text-red-500'>{errorMessage}</p>} */}
+          <h2>All Movies</h2>
           {
             isLoading ? (
               <Spinner />
